5a9ecfd3129fdf032b25d0198355deb26871b8c4
Park Billy <rtlink.park@gmail.com>
2019-07-08 09:56:18 +0900

    install dependency

---
+++ src/renderer/components/Home/index.vue
@@ -0,0 +1,17 @@
+<template>
+
+  <div>
+    <span>{{ new Date() | moment("dddd, MMMM Do YYYY") }}</span>
+  </div>
+
+</template>
+
+<script>
+	export default {
+		name: "index.vue"
+	}
+</script>
+
+<style scoped>
+
+</style>
